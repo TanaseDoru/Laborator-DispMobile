3465326bfb631aab3a971447ed39591427a3acda
Tanase Doru <tanase.doru21@gmail.com>
2026-03-25 13:50:00 +0200

    .

---
+++ Lab114A/app/src/main/java/ro/ase/lab114a/DateConverter.java
@@ -0,0 +1,30 @@
+package ro.ase.lab114a;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+public class DateConverter {
+
+    @TypeConverter
+    public Date fromTimeStamp(Long value)
+    {
+        return value == null ? null : new Date(value);
+
+
+
+
+    }
+    @TypeConverter
+    public Long dateToTimeStamp(Date date)
+    {
+        if(date == null)
+        {
+            return null;
+        }
+        return date.getTime();
+
+    }
+
+
+}
--- Lab114A/app/src/main/java/ro/ase/lab114a/StudentiDB.java
+++ Lab114A/app/src/main/java/ro/ase/lab114a/StudentiDB.java
@@ -2,8 +2,10 @@ package ro.ase.lab114a;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 @Database(entities = {Student.class}, version = 1, exportSchema = false)
+@TypeConverters(DateConverter.class)
 public abstract class StudentiDB extends RoomDatabase {
     
 
