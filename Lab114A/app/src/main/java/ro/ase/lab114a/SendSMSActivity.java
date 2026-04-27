c75cb86c24bc6161ee68f9ead789b62e1f47587b
Tanase Doru <tanase.doru21@gmail.com>
2026-04-27 12:20:31 +0300

    .

--- Lab114A/app/src/main/java/ro/ase/lab114a/SendSMSActivity.java
+++ Lab114A/app/src/main/java/ro/ase/lab114a/SendSMSActivity.java
@@ -1,6 +1,10 @@
 package ro.ase.lab114a;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,17 @@ public class SendSMSActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText etText = findViewById(R.id.editTextMessage);
+        EditText etPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        Spinner spinnerContacts = findViewById(R.id.spinnerContacts);
+        Button btnSend = findViewById(R.id.btnSend);
+
+        String[] contacts = {"1 Gigel 0721345678", "2 Dorel 0712346587"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                contacts);
+        spinnerContacts.setAdapter(adapter);
+            
     }
 }
